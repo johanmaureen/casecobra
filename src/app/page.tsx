9e4667e950b6397dee8a911553334cdf734824c2
johanrussouw <jrussouw610505@gmail.com>
2025-11-12 17:16:37 +0200

    landing page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Check, Star } from "lucide-react";
 import MaxWidtWrapper from "./components/MaxWidtWrapper";
+import Phone from "./components/Phone";
 
 export default function Home() {
   return (
@@ -21,6 +23,76 @@ export default function Home() {
                 CaseCobra alllows allows you to protect your memories, not just
                 your phone case
               </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex items-start space-x-4">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    High-quality printing that won't fade
+                  </li>
+                  <li className="flex items-start space-x-4">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex items-start space-x-4">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="useer image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="useer image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="useer image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="useer image"
+                  />
+                  <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-5.jpg"
+                    alt="useer image"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 lg:mx-0 h-fit">
+            <div className="relative md:max-w-xl">
+              <img
+                src="/your-image.png"
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+              />
+              <img
+                src="/line.png"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidtWrapper>
