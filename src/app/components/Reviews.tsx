a1422368ee7884a5cf7b47820137b24ef163b445
johanrussouw <jrussouw610505@gmail.com>
2025-11-15 16:46:06 +0200

    reviews and animation

--- src/app/components/Reviews.tsx
+++ src/app/components/Reviews.tsx
@@ -1,3 +1,98 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import MaxWidtWrapper from "./MaxWidtWrapper";
+import { useInView } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+const PHONES = [
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+    if (!result[index]) {
+      result[index] = [];
+    }
+
+    result[index].push(array[i]);
+  }
+
+  return result;
+}
+
+function ReviewColumn({
+  reviews,
+  className,
+  reviewwClassName,
+  msPerPixel = 0,
+}: {
+  reviews: string[];
+  className?: string;
+  reviewwClassName?: (reviewIndex: number) => string;
+  msPerPixel?: number;
+}) {
+  const columnRef = useRef<HTMLDivElement | null>(null);
+  const [columnHeigth, setColumnHeight] = useState(0);
+
+  useEffect(() => {
+    if (!columnRef.current) return;
+    const resizeObserver = new window.ResizeObserver(() => {
+      setColumnHeight(columnRef.current?.offsetHeight ?? 0);
+    });
+    resizeObserver.observe(columnRef.current);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={columnRef}
+      className={cn("animate-marquee space-y-8 py-4", className)}
+      style={{ "--marquee-duration": duration } as React.CSSProperties}
+    ></div>
+  );
+}
+
+function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: true, amount: 0.4 });
+  const colunms = splitArray(PHONES, 3);
+  const column1 = colunms[0];
+  const column2 = colunms[2];
+  const column3 = splitArray(colunms[2], 2);
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx4 grid h-196 max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    >
+      {isInView ? (
+        <>
+          <ReviewColumn />
+        </>
+      ) : null}
+    </div>
+  );
+}
+
 export function Reviews() {
-  return <div></div>;
+  return (
+    <MaxWidtWrapper className="relative max-w-5xl">
+      <img
+        aria-hidden="true"
+        src="/what-people-are-buying.png"
+        className="absolute select-none hidden xl:block -left-32 top-1/3"
+      />
+      <ReviewGrid />
+    </MaxWidtWrapper>
+  );
 }
